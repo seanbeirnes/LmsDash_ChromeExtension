f1c792946b40c7d535c3ed745bbbae75aaf5da0b
Sean Beirnes <127370575+seanbeirnes@users.noreply.github.com>
2024-07-07 18:17:56 -0400

    Removed ScanModeDropdown for non-admin user

--- src/SidePanel/features/CoursesScanner/components/CourseScanSelectCourse.jsx
+++ src/SidePanel/features/CoursesScanner/components/CourseScanSelectCourse.jsx
@@ -16,9 +16,9 @@ function CourseScanSelectCourse({scanType, setScanType, setCourseIds}) {
   return (
     <PrimaryCard fixedWidth={true}>
       <div className="grid grid-cols-1 grid-flow-row start justify-start content-start gap-2">
-        <h3 className="text-gray-700 text-xl text-center">Select Course(s)</h3>
+        <h3 className="text-gray-700 text-xl text-center">{appState.isAdmin ? "Select Course(s)" : "Selected Course"}</h3>
         <div className={`flex flex-col gap-2`}>
-          <ScanModeDropdown value={scanType[0]} onChange={updateScanType}/>
+          {appState.isAdmin && (<ScanModeDropdown value={scanType[0]} onChange={updateScanType}/>)}
           {scanType[0] === "single-course" &&
             (
               <SelectCourse courseId={appState.activeTabCourseId} setCourseIds={setCourseIds} />
