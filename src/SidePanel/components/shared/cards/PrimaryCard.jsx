3488c33c8934f695e2b28f199d581d588a3cd75f
Sean Beirnes <127370575+seanbeirnes@users.noreply.github.com>
2024-08-17 21:15:15 -0400

    Added top info component to ResutlsView

--- src/SidePanel/components/shared/cards/PrimaryCard.jsx
+++ src/SidePanel/components/shared/cards/PrimaryCard.jsx
@@ -1,7 +1,7 @@
-function PrimaryCard({children, className = "", fixedWidth=true})
+function PrimaryCard({children, className = "", fixedWidth=true, minHeight = true})
 {
   return (
-    <div className={`grid grid-cols-1 grid-flow-row min-h-72 min-w-72 bg-gray-50 p-4 rounded-lg ${fixedWidth ? "w-72" : ""} ${className}`}>
+    <div className={`grid grid-cols-1 grid-flow-row ${minHeight ? "min-h-72" : ""} min-w-72 bg-gray-50 p-4 rounded-lg ${fixedWidth ? "w-72" : ""} ${className}`}>
       {children}
     </div>
   )
--- src/SidePanel/features/CoursesScanner/views/ResultsView/ResultsView.jsx
+++ src/SidePanel/features/CoursesScanner/views/ResultsView/ResultsView.jsx
@@ -1,6 +1,13 @@
 import PrimaryCardLayout from "../../../../components/shared/cards/PrimaryCardLayout.jsx";
 import useTaskById from "../../../../hooks/useTaskById.js";
 import ProgressSpinner from "../../../../components/shared/progress/ProgressSpinner.jsx";
+import PrimaryCard from "../../../../components/shared/cards/PrimaryCard.jsx";
+import * as Progress from "@radix-ui/react-progress";
+import Task from "../../../../../shared/models/Task.js";
+import ButtonPrimaryDanger from "../../../../components/shared/buttons/ButtonPrimaryDanger.jsx";
+import ButtonPrimary from "../../../../components/shared/buttons/ButtonPrimary.jsx";
+import {DownloadIcon} from "@radix-ui/react-icons";
+import IconButton from "../../../../components/shared/buttons/IconButton.jsx";
 
 function ResultsView({taskId, scanAgainCallback})
 {
@@ -15,8 +22,34 @@ function ResultsView({taskId, scanAgainCallback})
 
   return (
     <PrimaryCardLayout>
-      <p>Results view...</p>
-      <button onClick={scanAgainCallback}>Button</button>
+      <PrimaryCard fixedWidth={false} minHeight={false} className="w-full">
+        <div className="grid grid-cols-1 grid-flow-row start justify-start content-start gap-2">
+          <h2
+            className="text-gray-700 text-xl text-center font-bold">{data.progressData.length > 0 ? data.progressData[0] : " "}</h2>
+          <Progress.Root className="relative overflow-hidden bg-gray-200 rounded-full w-full h-6 shadow-inner"
+                         style={{transform: "translateZ(0)"}}
+                         value={data.progress ? data.progress : 0}>
+            <Progress.Indicator className="bg-blue-400 w-full h-full transition-transform duration-500 ease-in-out"
+                                style={{transform: `translateX(-${100 - (data.progress ? data.progress : 0)}%)`}}/>
+            <span
+              className="absolute top-0 w-full h-full text-center text-gray-700">{data.progress ? data.progress : 0}%</span>
+          </Progress.Root>
+          <p
+            className="text-gray-700 text-base text-left mb-4">{`Items found in ${data.resultsData.length} course(s).`}</p>
+        </div>
+        <div className="justify-self-center self-end w-full min-h16 flex justify-between items-center gap-4">
+          <div className="hidden sm:block min-w-12">
+          </div>
+          <div className="w-full max-w-sm">
+            <ButtonPrimary onClick={scanAgainCallback}>New Scan</ButtonPrimary>
+          </div>
+          <div>
+            <IconButton animated={true}>
+              <DownloadIcon className="w-10 h-10 p-2"/>
+            </IconButton>
+          </div>
+        </div>
+      </PrimaryCard>
       <p>{JSON.stringify(data)}</p>
     </PrimaryCardLayout>
   )
