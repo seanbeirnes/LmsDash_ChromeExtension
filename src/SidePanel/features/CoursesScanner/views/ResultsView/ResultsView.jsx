4d58be1b3602d5306a2c8518817a4a3e8df8d643
Sean Beirnes <127370575+seanbeirnes@users.noreply.github.com>
2024-08-18 23:02:17 -0400

    Completed results view details modal

--- src/SidePanel/features/CoursesScanner/views/ResultsView/ResultsView.jsx
+++ src/SidePanel/features/CoursesScanner/views/ResultsView/ResultsView.jsx
@@ -10,6 +10,7 @@ import CourseScanResult from "./CourseScanResult.jsx";
 import * as Tooltip from "@radix-ui/react-tooltip";
 import {useState} from "react";
 import * as Dialog from "@radix-ui/react-dialog";
+import * as Tabs from "@radix-ui/react-tabs";
 
 function ResultsView({taskId, scanAgainCallback})
 {
@@ -98,22 +99,45 @@ function ResultsView({taskId, scanAgainCallback})
     <Dialog.Portal>
       <Dialog.Overlay className="fixed inset-0 w-dvw h-dvh bg-gray-700 opacity-50" />
       <Dialog.Content onInteractOutside={() => setCurDetails(null)} className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-dvw max-w-[85dvw] sm:max-w-lg max-h-[85dvh] min-h-32 p-8 text-2xl bg-white shadow-lg rounded">
-        <Dialog.Title className="text-gray-700 font-bold mb-2">
+        <Dialog.Title className="text-gray-700 font-bold mb-2 text-center">
           Scan Result Details
         </Dialog.Title>
-        <Dialog.Description className="text-base text-gray-700">
-          {curDetails.name}
+        <Dialog.Description className="text-sm text-gray-700">
+          {curDetails !== null && curDetails.name}
         </Dialog.Description>
-        <div>
-          <p>{JSON.stringify(curDetails)}</p>
-        </div>
-        <div className="w-full flex justify-end">
-          <div className="max-w-32">
-            <Dialog.Close asChild>
-              <ButtonPrimary onClick={() => setCurDetails(null)}>OK</ButtonPrimary>
-            </Dialog.Close>
-          </div>
-        </div>
+        <Tabs.Root className="flex flex-col w-full h-80" defaultValue="tab1">
+          <Tabs.List className="shrink-0 flex border-b border-gray-200" aria-label="View details for course item">
+            <Tabs.Trigger value="tab1" className="h-10 flex-1 flex items-center justify-center text-base select-none text-gray-700 hover:text-blue-500 data-[state=active]:text-blue-600 data-[state=active]:shadow-[inset_0_-2px_0_0] data-[state=active]:shadow-current cursor-default">Previews</Tabs.Trigger>
+            <Tabs.Trigger value="tab2" className="h-10 flex-1 flex items-center justify-center text-base select-none text-gray-700 hover:text-blue-500 data-[state=active]:text-blue-600 data-[state=active]:shadow-[inset_0_-2px_0_0] data-[state=active]:shadow-current cursor-default">Details</Tabs.Trigger>
+          </Tabs.List>
+          <Tabs.Content value="tab1" className="grow">
+            {(curDetails !== null && curDetails.previews.length > 0) && <div
+              className="w-full h-full p-4 flex flex-col overflow-y-scroll bg-gray-200 rounded-b shadow-inner">
+              {
+                curDetails.previews.map((preview, index) => {
+                  return <div className="w-full break-all" key={"preview-" + index}>
+                    <p  className="text-sm font-mono">{preview[0] ? preview[0] : ""}<span className="py-1 rounded bg-blue-200 font-bold">{preview[1]}</span>{preview[2] ? preview[2] : ""}</p>
+                    <hr className="border-gray-400 my-2"/>
+                  </div>
+                })
+              }
+            </div>}
+          </Tabs.Content>
+          <Tabs.Content value="tab2" className="grow">
+            {curDetails && <div className="w-full h-full text-sm p-4 flex flex-col break-words rounded-b shadow-inner">
+              <p className="text-base text-gray-700"><span className="font-bold">Name:</span> {curDetails.name}</p>
+              <p className="text-base text-gray-700"><span className="font-bold">ID:</span> {curDetails.id}</p>
+              <p className="text-base text-gray-700"><span
+                className="font-bold">Published:</span> {curDetails.published ? "TRUE" : "FALSE"}</p>
+              <p className="text-base break-all text-gray-700"><span className="font-bold">URL:</span> <a href={curDetails.url}
+                                                                                                target="_blank"
+                                                                                                className="text-blue-600 hover:text-blue-500 hover:underline active:text-blue-400">{curDetails.url}</a>
+              </p>
+              <p className="text-base text-gray-700"><span className="font-bold">Matches:</span> {curDetails.matches.toString().replaceAll(/[\[\]]/g,"").replaceAll(",",", ")}
+              </p>
+            </div>}
+          </Tabs.Content>
+        </Tabs.Root>
         <Dialog.Close asChild>
           <IconButton animated={false} onClick={() => setCurDetails(null)} className="text-gray-700 hover:text-white hover:bg-gray-700 absolute top-3 right-3 appearance-none rounded-full">
             <Cross2Icon className="w-8 h-8 p-2" />
