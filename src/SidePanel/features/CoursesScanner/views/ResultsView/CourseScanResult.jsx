337655b0c5cde12e1202a1f1bc46ec9befbdd30b
Sean Beirnes <127370575+seanbeirnes@users.noreply.github.com>
2024-08-18 20:45:38 -0400

    Moved course info pills into collapsible

--- src/SidePanel/features/CoursesScanner/views/ResultsView/CourseScanResult.jsx
+++ src/SidePanel/features/CoursesScanner/views/ResultsView/CourseScanResult.jsx
@@ -19,7 +19,7 @@ function CourseScanResult({id, name, courseCode, sisCourseId, published, url, it
   return (
     <PrimaryCard fixedWidth={false} minHeight={false} className="w-full">
       <Collapsible.Root open={open} onOpenChange={setOpen}>
-        <div className="flex flex-row items-start gap-1">
+        <div className="flex flex-row items-center gap-1 mb-2">
           <h3 className="basis-full text-xl text-Left">
             <a href={url} target="_blank" title="Link to course"
                className="text-blue-600 hover:text-blue-500 hover:underline active:text-blue-400">{name}</a>
@@ -32,55 +32,63 @@ function CourseScanResult({id, name, courseCode, sisCourseId, published, url, it
               <CaretSortIcon className="w-9 h-9"/>}
           </Collapsible.Trigger>
         </div>
-        <div
-          className="w-full sm:w-fit flex flex-col sm:flex-row flex-wrap justify-start items-center content-center gap-2">
-          {published ?
-            <p
-              className="w-full sm:w-fit px-4 py-1 text-sm text-green-600 bg-green-200 font-bold text-center rounded-full">Published</p>
-            :
-            <p
-              className="w-full sm:w-fit px-4 py-1 text-sm text-red-600 bg-red-200 font-bold text-center rounded-full">Unpublished</p>
-          }
-          {
-            courseCode &&
-            <p className="w-full sm:w-fit px-4 py-1 text-sm text-gray-400 bg-gray-100 text-center rounded-full"><span
-              className="inline-block font-bold">Course Code:</span> {courseCode}</p>
-          }
-          {
-            sisCourseId &&
-            <p className="w-full sm:w-fit px-4 py-1 text-sm text-gray-400 bg-gray-100 text-center rounded-full"><span
-              className="inline-block font-bold">SIS ID:</span> {sisCourseId}</p>
-          }
-          {
-            id &&
-            <p className="w-full sm:w-fit px-4 py-1 text-sm text-gray-400 bg-gray-100 text-center rounded-full"><span
-              className="inline-block font-bold">ID:</span> {id}</p>
-          }
-        </div>
         <Collapsible.Content>
+          <div
+            className="w-full sm:w-fit flex flex-col sm:flex-row flex-wrap justify-start items-center content-center gap-2">
+            {published ?
+              <p
+                className="w-full sm:w-fit px-4 py-1 text-sm text-green-600 bg-green-200 font-bold text-center rounded-full">Published</p>
+              :
+              <p
+                className="w-full sm:w-fit px-4 py-1 text-sm text-red-600 bg-red-200 font-bold text-center rounded-full">Unpublished</p>
+            }
+            {
+              courseCode &&
+              <p className="w-full sm:w-fit px-4 py-1 text-sm text-gray-400 bg-gray-100 text-center rounded-full"><span
+                className="inline-block font-bold">Course Code:</span> {courseCode}</p>
+            }
+            {
+              sisCourseId &&
+              <p className="w-full sm:w-fit px-4 py-1 text-sm text-gray-400 bg-gray-100 text-center rounded-full"><span
+                className="inline-block font-bold">SIS ID:</span> {sisCourseId}</p>
+            }
+            {
+              id &&
+              <p className="w-full sm:w-fit px-4 py-1 text-sm text-gray-400 bg-gray-100 text-center rounded-full"><span
+                className="inline-block font-bold">ID:</span> {id}</p>
+            }
+          </div>
           {(items.announcement?.length > 0) &&
-            <CourseItemResultsSection icon={<MegaphoneSimpleIcon className="w-6 h-6 text-gray-400"/>} title="Announcements" items={items.announcement}/>
+            <CourseItemResultsSection icon={<MegaphoneSimpleIcon className="w-6 h-6 text-gray-400"/>}
+                                      title="Announcements" items={items.announcement}/>
           }
           {(items.assignment?.length > 0) &&
-            <CourseItemResultsSection icon={<PencilLineIcon className="w-6 h-6 text-gray-400"/>} title="Assignments" items={items.assignment}/>
+            <CourseItemResultsSection icon={<PencilLineIcon className="w-6 h-6 text-gray-400"/>} title="Assignments"
+                                      items={items.assignment}/>
           }
           {(items.courseNavLink?.length > 0) &&
-            <CourseItemResultsSection icon={<LinkSimpleIcon className="w-6 h-6 text-gray-400"/>} title="Course Navigation Links" items={items.courseNavLink}/>
+            <CourseItemResultsSection icon={<LinkSimpleIcon className="w-6 h-6 text-gray-400"/>}
+                                      title="Course Navigation Links" items={items.courseNavLink}/>
           }
           {(items.discussion?.length > 0) &&
-            <CourseItemResultsSection icon={<ChatCircleTextIcon className="w-6 h-6 text-gray-400"/>} title="Discussions" items={items.discussion}/>
+            <CourseItemResultsSection icon={<ChatCircleTextIcon className="w-6 h-6 text-gray-400"/>} title="Discussions"
+                                      items={items.discussion}/>
           }
           {(items.file?.length > 0) &&
-            <CourseItemResultsSection icon={<FileIcon className="w-6 h-6 text-gray-400"/>} title="File Names" items={items.file}/>
+            <CourseItemResultsSection icon={<FileIcon className="w-6 h-6 text-gray-400"/>} title="File Names"
+                                      items={items.file}/>
           }
           {(items.moduleLink?.length > 0) &&
-            <CourseItemResultsSection icon={<LinkSimpleIcon className="w-6 h-6 text-gray-400"/>} title="Module Links" items={items.moduleLink}/>
+            <CourseItemResultsSection icon={<LinkSimpleIcon className="w-6 h-6 text-gray-400"/>} title="Module Links"
+                                      items={items.moduleLink}/>
           }
           {(items.page?.length > 0) &&
-            <CourseItemResultsSection icon={<FileTextIcon className="w-6 h-6 text-gray-400"/>} title="Pages" items={items.page}/>
+            <CourseItemResultsSection icon={<FileTextIcon className="w-6 h-6 text-gray-400"/>} title="Pages"
+                                      items={items.page}/>
           }
           {(items.syllabus?.length > 0) &&
-            <CourseItemResultsSection icon={<ClipboardTextIcon className="w-6 h-6 text-gray-400"/>} title="Syllabus" items={items.syllabus}/>
+            <CourseItemResultsSection icon={<ClipboardTextIcon className="w-6 h-6 text-gray-400"/>} title="Syllabus"
+                                      items={items.syllabus}/>
           }
         </Collapsible.Content>
       </Collapsible.Root>
